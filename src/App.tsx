d3b7dc192658ed21039d3c68acd5203d7d6693c9
Michael Lustig <tie.snuffle-0i@icloud.com>
2021-10-29 19:35:58 -0400

    Ripped out the good bits
    need interval
    need firebase
    need cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 import Home from "./Home";
 
 import * as anchor from "@project-serum/anchor";
-import { clusterApiUrl } from "@solana/web3.js";
+import { clusterApiUrl, Commitment } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   getPhantomWallet,
@@ -21,6 +21,7 @@ import {
 
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 import { createTheme, ThemeProvider } from "@material-ui/core";
+import { getCandyMachineState } from "./candy-machine";
 
 const treasury = new anchor.web3.PublicKey(
   process.env.REACT_APP_TREASURY_ADDRESS!
@@ -44,62 +45,68 @@ const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
 const txTimeout = 30000; // milliseconds (confirm this works for your project)
 
 const theme = createTheme({
-    palette: {
-        type: 'dark',
+  palette: {
+    type: "dark",
+  },
+  overrides: {
+    MuiButtonBase: {
+      root: {
+        justifyContent: "flex-start",
+      },
     },
-    overrides: {
-        MuiButtonBase: {
-            root: {
-                justifyContent: 'flex-start',
-            },
-        },
-        MuiButton: {
-            root: {
-                textTransform: undefined,
-                padding: '12px 16px',
-            },
-            startIcon: {
-                marginRight: 8,
-            },
-            endIcon: {
-                marginLeft: 8,
-            },
-        },
+    MuiButton: {
+      root: {
+        textTransform: undefined,
+        padding: "12px 16px",
+      },
+      startIcon: {
+        marginRight: 8,
+      },
+      endIcon: {
+        marginLeft: 8,
+      },
     },
+  },
 });
 
+const ___refactor_me = async () => {
+  const wallet = new anchor.Wallet(anchor.web3.Keypair.generate());
+  const candyMachineId = new anchor.web3.PublicKey(
+    "HoE3Y8Edc6XaYuobWsSpp2YzGUJaeiYiJdCUAoykhzWv"
+  );
+  const httpUri = "https://explorer-api.devnet.solana.com/";
+  const requiredTxFinalization: Commitment = "confirmed";
+  const connection = new anchor.web3.Connection(
+    httpUri,
+    requiredTxFinalization
+  );
+  const {
+    candyMachine,
+    goLiveDate,
+    itemsAvailable,
+    itemsRemaining,
+    itemsRedeemed,
+  } = await getCandyMachineState(
+    wallet as anchor.Wallet,
+    candyMachineId,
+    connection
+  );
+
+  console.log({
+    candyMachine,
+    goLiveDate,
+    itemsAvailable,
+    itemsRemaining,
+    itemsRedeemed,
+  });
+};
+
 const App = () => {
-  const endpoint = useMemo(() => clusterApiUrl(network), []);
+  useEffect(() => {
+    ___refactor_me();
+  }, []);
 
-  const wallets = useMemo(
-    () => [
-        getPhantomWallet(),
-        getSlopeWallet(),
-        getSolflareWallet(),
-        getSolletWallet({ network }),
-        getSolletExtensionWallet({ network })
-    ],
-    []
-  );
-
-  return (
-      <ThemeProvider theme={theme}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-              <Home
-                candyMachineId={candyMachineId}
-                config={config}
-                connection={connection}
-                startDate={startDateSeed}
-                treasury={treasury}
-                txTimeout={txTimeout}
-              />
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </ThemeProvider>
-  );
+  return <div>dude what is this</div>;
 };
 
 export default App;
